55dcfe771d605f2d65bff61f6fc67797749e5e75
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2024-03-10 13:33:13 +0500

    npm run build with cache:'no-store'

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,21 +6,22 @@ interface User {
 }
 
 const UsersPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users' ,
-    { next: { revalidate: 10 }})
+    const res = await fetch('https://jsonplaceholder.typicode.com/users',{cache: 'no-store'},
+    )
     const users: User[] = await res.json();
 
 
-  return (
-    <>
-        <h1>Users</h1>
-        <ul>
-            {users.map(user => <li key={user.id} >
-                {user.name}
-            </li>)}
-        </ul>
-    </>
-  )
+    return (
+        <>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map(user => <li key={user.id} >
+                    {user.name}
+                </li>)}
+            </ul>
+        </>
+    )
 }
 
 export default UsersPage
